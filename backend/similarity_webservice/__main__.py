6f8dcc738cbe66f73525b3bf7487cd07bf3530e3
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2024-01-22 14:38:19 +0100

    remove overconfiguration

--- backend/similarity_webservice/__main__.py
+++ backend/similarity_webservice/__main__.py
@@ -1,4 +1,4 @@
-from similarity_webservice.app import create_app
+from similarity_webservice.app import app
 
 import click
 
@@ -6,11 +6,7 @@ import click
 @click.command()
 @click.option("--host", default="localhost", help="Host to bind to.", show_default=True)
 @click.option("--port", default=5000, help="Port to bind to.", show_default=True)
-@click.option(
-    "--config", default="config.yaml", help="Path to config file.", show_default=True
-)
-def main(host, port, config):
-    app = create_app(config)
+def main(host, port):
     app.run(host=host, port=port)
 
 
--- backend/similarity_webservice/app.py
+++ backend/similarity_webservice/app.py
@@ -5,46 +5,48 @@ import flask
 import logging
 
 
-def create_app(config_path: Optional[str] = None) -> flask.Flask:
-    """Create the flask app that will be used to run the similarity webservice."""
+# Create a logger instance for our server.
+logger = logging.getLogger("similarity_webservice")
 
-    # Create a logger instance for our server.
-    logger = logging.getLogger("similarity_webservice")
+# Create the flask app that will returned by this function
+app = flask.Flask("similarity_webservice")
 
-    # Create the flask app that will returned by this function
-    app = flask.Flask("similarity_webservice")
+# Extract the config from environment variables and inject it into falsk
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///similarity_webservice.db"
 
-    # Extract the config from environment variables and inject it into falsk
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///similarity_webservice.db"
+# Initialize the database
+db.init_app(app)
 
-    # Initialize the database
-    db.init_app(app)
 
-    @app.route("/api/collection/create", methods=["POST"])
-    def create_collection():
-        raise NotImplementedError()
+@app.route("/api/collection/create", methods=["POST"])
+def create_collection():
+    raise NotImplementedError()
 
-    @app.route("/api/collection/delete", methods=["DELETE"])
-    def delete_collection():
-        raise NotImplementedError()
 
-    @app.route("/api/collection/list", methods=["GET"])
-    def list_collections():
-        raise NotImplementedError()
+@app.route("/api/collection/delete", methods=["DELETE"])
+def delete_collection():
+    raise NotImplementedError()
 
-    @app.route("/api/collection/update", methods=["PUT"])
-    def update_collection():
-        raise NotImplementedError()
 
-    @app.route("/api/collection/finetune", methods=["POST"])
-    def finetune_collection():
-        raise NotImplementedError()
+@app.route("/api/collection/list", methods=["GET"])
+def list_collections():
+    raise NotImplementedError()
 
-    @app.route("/api/search", methods=["GET"])
-    def search():
-        raise NotImplementedError()
 
-    with app.app_context():
-        db.create_all()
+@app.route("/api/collection/update", methods=["PUT"])
+def update_collection():
+    raise NotImplementedError()
 
-    return app
+
+@app.route("/api/collection/finetune", methods=["POST"])
+def finetune_collection():
+    raise NotImplementedError()
+
+
+@app.route("/api/search", methods=["GET"])
+def search():
+    raise NotImplementedError()
+
+
+with app.app_context():
+    db.create_all()
