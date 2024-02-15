e226bdfe5eef7afec7944821a1daf6641a72173d
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2024-02-15 13:57:03 +0100

    Add vision.py

---
+++ backend/similarity_webservice/vision.py
@@ -0,0 +1,10 @@
+def finetune_model(id: str):
+    """Finetune a model with a given collection."""
+
+    raise NotImplementedError()
+
+
+def similarity_search(id: str, image):
+    """Search for similar images in a given collection."""
+
+    raise NotImplementedError()
