326e2be8d301ef1ae45011a09d6a8413281491d6
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2024-02-16 10:43:04 +0100

    Add file input box

--- frontend/src/routes/Search.svelte
+++ frontend/src/routes/Search.svelte
@@ -1,7 +1,24 @@
 <script>
-  import { Dropzone } from "flowbite-svelte";
+  import { Dropzone, Gallery } from "flowbite-svelte";
+
+  let query = [];
 </script>
 
-<Dropzone>
+<div class="grid grid-cols-2 gap-2">
   <!-- https://flowbite-svelte.com/docs/forms/file-input -->
-</Dropzone>
+  <div>
+    <Dropzone id="dropzone">
+      {#if query.length === 0}
+        <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+        <p class="text-xs text-gray-500 dark:text-gray-400">PNG, JPG or GIF</p>
+      {:else}
+        Foo
+      {/if}
+    </Dropzone>
+  </div>
+  <div>
+    <Gallery>
+      <!-- https://flowbite-svelte.com/docs/media/gallery -->
+    </Gallery>
+  </div>
+</div>
