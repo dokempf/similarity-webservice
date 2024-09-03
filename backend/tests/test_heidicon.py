a51fa674eb02f7a2fade8ec3bfb18cecb50e9b4a
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2024-09-03 17:23:41 +0200

    Correct tag in heidicon unit test

--- backend/tests/test_heidicon.py
+++ backend/tests/test_heidicon.py
@@ -2,6 +2,6 @@ from similarity_webservice.heidicon import extract_heidicon_content
 
 
 def test_extract_heidicon_content():
-    images = extract_heidicon_content("NHDP Similarity Search")
+    images = extract_heidicon_content("DANAM Similarity Search")
 
     assert len(images) > 0
